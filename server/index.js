41671988ae7255a17e2b1a79b269f7596e1c5d80
zMed-tech <badache.amn@gmail.com>
2022-01-26 17:30:08 +0100

    feat: import the apollo server and start it

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const sequelize = require("./database");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./graphql/index");
 
 const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
 
 app.use(bodyParser.json());
 app.use(cors());
+
+server.start().then(() => {
+  server.applyMiddleware({ app });
+});
 
 sequelize.sync().then(() => {
   app.listen(5000, () => {
