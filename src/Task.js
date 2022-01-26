2e9092f5da1749fd54660dc03f0492526fb3b2b5
zMed-tech <badache.amn@gmail.com>
2022-01-27 00:46:14 +0100

    feat: add methode to delete this task using mutation

--- src/Task.js
+++ src/Task.js
@@ -1,15 +1,35 @@
 import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const deleteTask = gql`
+  mutation deleteTask($deleteTaskId: ID) {
+    deleteTask(id: $deleteTaskId) {
+      id
+    }
+  }
+`;
 
 const Task = (props) => {
   const [finished, setFinished] = useState(false);
-  const handleChange = (e) => {
+  const [mutateDeleteTask, { loading }] = useMutation(deleteTask);
+  const handleChange = async (e) => {
     if (e.target.id === "finished") {
       setFinished(!finished);
+    } else if (e.target.id === "delete") {
+      await mutateDeleteTask({ variables: { deleteTaskId: props.task.id } });
+      props.refetch();
     }
   };
 
   return (
     <div className="flex flex-col border-y py-3">
+      {loading ? (
+        <div>
+          <span>Loading ...</span>
+        </div>
+      ) : (
+        ""
+      )}
       <div className="mx-auto">
         <span
           className={`text-2xl text-indigo-600 font-bold 
@@ -43,6 +63,8 @@ const Task = (props) => {
           Edit
         </button>
         <button
+          id="delete"
+          onClick={handleChange}
           className="mx-2 bg-orange-500 
         text-white 
         font-bold 
