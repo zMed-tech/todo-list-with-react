788fc8d7974e68ec540179a7881c60a7ea28bb63
zMed-tech <badache.amn@gmail.com>
2022-01-26 14:36:41 +0100

    feat: create the todo component with some style

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,26 @@
 const Todo = () => {
-  return <div></div>;
+  return (
+    <div className="my-2 flex flex-col bg-white h-full p-5 shadow shadow-gray">
+      <div className="my-4 mx-auto">
+        <span className="text-xl font-bold text-indigo-900">My Todo</span>
+      </div>
+
+      <div className="my-4 border py-1">
+        <input className="mx-4 outline-0" placeholder="My task for today" />
+        <button
+          className="bg-indigo-500 
+                            py-1 px-2 
+                            text-white 
+                            font-bold
+                            rounded-sm
+                            mr-1
+                            "
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
