fad25664168aa9675b41008e5b3ca134722c0b1f
zMed-tech <badache.amn@gmail.com>
2022-01-27 14:03:51 +0100

    feat update task when edit button are clicked

--- src/EditTask.js
+++ src/EditTask.js
@@ -1,62 +1,130 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setOpenEdit, setMakeRefetch } from "./store/reducers/task";
+import { useMutation, gql } from "@apollo/client";
+
+const updateTask = gql`
+  mutation updateTask(
+    $updateTaskId: ID
+    $title: String
+    $description: String
+    $finished: Boolean
+    $createdAt: String
+    $finishedAt: String
+    $updateAt: String
+  ) {
+    updateTask(
+      id: $updateTaskId
+      title: $title
+      description: $description
+      finished: $finished
+      created_at: $createdAt
+      finished_at: $finishedAt
+      update_at: $updateAt
+    ) {
+      id
+    }
+  }
+`;
 
 const EditTask = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [setMaxChar, setsetMaxChar] = useState(300);
+  const { openEdit, id, makeRefetch } = useSelector((state) => state.task);
+  const dispatch = useDispatch();
+  const [mutateUpdateTask, { loading }] = useMutation(updateTask);
 
   useEffect(() => {
     setsetMaxChar(300 - description.length);
   }, [description]);
 
-  const handleChange = (e) => {
+  const handleChange = async (e) => {
     if (e.target.localName === "input") {
       setTitle(e.target.value);
     } else if (e.target.localName === "textarea") {
       setDescription(e.target.value);
+    } else if (e.target.id === "edit") {
+      if (title !== "" && description !== "") {
+        await mutateUpdateTask({
+          variables: {
+            updateTaskId: id,
+            title,
+            description,
+            updateAt: Date.now() + "",
+          },
+        });
+        setTitle("");
+        setDescription("");
+        dispatch(setOpenEdit(false));
+        dispatch(setMakeRefetch(!makeRefetch));
+      }
     }
   };
 
-  return (
-    <div
-      className="h-screen w-screen fixed flex
-     justify-center items-center z-10
-    
-     "
-      style={{ backgroundColor: "rgba(0,0,0,0.2)" }}
-    >
-      <div className="bg-white p-3 rounded-lg">
-        <div className="border p-1">
-          <input
-            className="outline-0"
-            placeholder="Title"
-            value={title}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="border p-1 my-2 relative">
-          <textarea
-            className="outline-0"
-            rows={7}
-            cols={45}
-            maxLength={300}
-            placeholder="Description"
-            value={description}
-            onChange={handleChange}
-          ></textarea>
-          <span className="absolute bottom-1 right-1 text-indigo-400">
-            {" "}
-            {setMaxChar}{" "}
-          </span>
-        </div>
-        <div className="">
-          <button className="w-full bg-indigo-700 text-white font-bold">
-            Edit
-          </button>
+  if (openEdit) {
+    return (
+      <div
+        className="h-screen w-screen fixed flex
+         justify-center items-center z-10
+        
+         "
+        style={{ backgroundColor: "rgba(0,0,0,0.2)" }}
+      >
+        <div className="bg-white p-3 rounded-lg">
+          <div className="flex justify-end mb-2">
+            {loading ? (
+              <div>
+                <span>Loading...</span>
+              </div>
+            ) : (
+              ""
+            )}
+            <button
+              className="bg-red-600 text-white font-bold rounded-xl px-2"
+              onClick={() => dispatch(setOpenEdit(false))}
+            >
+              Close
+            </button>
+          </div>
+          <div className="border p-1">
+            <input
+              className="outline-0"
+              placeholder="Title"
+              value={title}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="border p-1 my-2 relative">
+            <textarea
+              className="outline-0"
+              rows={7}
+              cols={45}
+              maxLength={300}
+              placeholder="Description"
+              value={description}
+              onChange={handleChange}
+            ></textarea>
+            <span className="absolute bottom-1 right-1 text-indigo-400">
+              {" "}
+              {setMaxChar}{" "}
+            </span>
+          </div>
+          <div className="">
+            <button
+              id="edit"
+              className="w-full bg-indigo-700 text-white font-bold"
+              onClick={handleChange}
+            >
+              Edit
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return "";
+  }
 };
 
 export default EditTask;
