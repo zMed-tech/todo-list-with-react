eedd06d9827ae5b9e0ea884cd82afb5ed11b512a
zMed-tech <badache.amn@gmail.com>
2022-01-27 15:01:15 +0100

    feat: add loading reducer to our store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import task from "./reducers/task";
+import loading from "./reducers/loading";
 
 export default configureStore({
-  reducer: { task },
+  reducer: { task, loading },
 });
